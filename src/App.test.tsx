09bb23c06f5a036aad7e885aa28dba91a4aef913
Daniel Russom <daniel.russom00@gmail.com>
2023-03-09 10:35:44 +0000

    Order app.test with new describe blocks

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,62 +3,69 @@ import userEvent from '@testing-library/user-event';
 import * as React from 'react';
 import App from './App';
 
-it('does not display content in the component container', () => {
-  render(<App/>);
 
-  const componentContainer = screen.queryByTestId("component-container");
+describe("App", () => {
 
-  expect(componentContainer).toBeEmptyDOMElement()
+  it('does not display content in the component container', () => {
+    render(<App />);
+
+    const componentContainer = screen.queryByTestId("component-container");
+
+    expect(componentContainer).toBeEmptyDOMElement()
+  });
+
+  describe("tree generator", () => {
+    it('contains a display button', () => {
+      render(<App />);
+
+      const treeGeneratorButton = screen.getByRole('button', { name: 'Christmas Tree Generator' });
+
+      expect(treeGeneratorButton).toBeInTheDocument();
+    });
+
+    it('is displayed', () => {
+      render(<App />);
+
+      const treeGeneratorNavigationButton = screen.getByRole('button', { name: 'Christmas Tree Generator' });
+      userEvent.click(treeGeneratorNavigationButton);
+
+      const generateTreeButton = screen.getByRole('button', { name: 'Generate Tree' });
+
+      expect(generateTreeButton).toBeInTheDocument();
+    });
+
+    it('is hidden', () => {
+      render(<App />);
+
+      const treeGeneratorNavigationButton = screen.getByRole('button', { name: 'Christmas Tree Generator' });
+      userEvent.click(treeGeneratorNavigationButton);
+      userEvent.click(treeGeneratorNavigationButton);
+
+      const componentContainer = screen.queryByTestId("component-container");
+
+      expect(componentContainer).toBeEmptyDOMElement()
+    });
+  })
+
+  describe("stack exercise", () => {
+    it('contains a display button', () => {
+      render(<App />);
+
+      const stackExerciseButton = screen.getByRole('button', { name: 'Stack Exercise' });
+
+      expect(stackExerciseButton).toBeInTheDocument();
+    });
+
+    it('is displayed', () => {
+      render(<App />);
+
+      const stackExerciseButton = screen.getByRole('button', { name: 'Stack Exercise' });
+      userEvent.click(stackExerciseButton);
+
+      const stackTitle = screen.getByText("Stack:");
+
+      expect(stackTitle).toBeInTheDocument();
+    });
+  });
 });
-
-it('contains a christmas tree generator button', () => {
-  render(<App/>);
-
-  const treeGeneratorButton = screen.getByRole('button', { name: 'Christmas Tree Generator' });
-
-  expect(treeGeneratorButton).toBeInTheDocument();
-});
-
-it('displays the treeGenerator', () => {
-  render(<App/>);
-
-  const treeGeneratorNavigationButton = screen.getByRole('button', { name: 'Christmas Tree Generator' });
-  userEvent.click(treeGeneratorNavigationButton);
-
-  const generateTreeButton = screen.getByRole('button', { name: 'Generate Tree' });
-
-  expect(generateTreeButton).toBeInTheDocument();
-});
-
-it('hides the treeGenerator', () => {
-  render(<App/>);
-
-  const treeGeneratorNavigationButton = screen.getByRole('button', { name: 'Christmas Tree Generator' });
-  userEvent.click(treeGeneratorNavigationButton); 
-  userEvent.click(treeGeneratorNavigationButton);
-
-  const componentContainer = screen.queryByTestId("component-container");
-
-  expect(componentContainer).toBeEmptyDOMElement()
-});
-
-it('contains a stack exercise button', () => {
-  render(<App/>);
-
-  const stackExerciseButton = screen.getByRole('button', { name: 'Stack Exercise' });
-
-  expect(stackExerciseButton).toBeInTheDocument();
-});
-
-it('displays the stack exercise', () => {
-  render(<App/>);
-
-  const stackExerciseButton = screen.getByRole('button', { name: 'Stack Exercise' });
-  userEvent.click(stackExerciseButton);
-
-  const stackTitle = screen.getByText("Stack:");
-
-  expect(stackTitle).toBeInTheDocument();
-});
-
 
