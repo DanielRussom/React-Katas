85a1fb71efc054575fb546593afd8bff6c8b2ae6
Daniel Russom <daniel.russom00@gmail.com>
2023-03-11 12:25:16 +0000

    Initialise rough outline of stack acceptance test

--- src/tree-generator/StackExercise.test.tsx
+++ src/tree-generator/StackExercise.test.tsx
@@ -1,6 +1,39 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import * as React from 'react';
 import StackExercise from "./StackExercise";
+
+describe("Acceptance test", () => {
+
+  it('has working push, pop, empty check, size, and peek functions', () => {
+    render(<StackExercise />);
+
+    let emptyCheckResult = screen.queryByText("The stack is empty");
+    expect(emptyCheckResult).toBeNull();
+
+    const emptyCheckButton = screen.getByRole('button', { name: 'Empty Check' });
+    userEvent.click(emptyCheckButton);
+    emptyCheckResult = screen.getByText("The stack is empty");
+    expect(emptyCheckResult).toBeInTheDocument();
+
+    // Run size == 0
+    
+    // Peek value
+
+    //Push two values
+
+    // Run empty check test
+
+    // Expect size == 2
+
+    // Peek value
+
+    // Pop a value
+
+    // Peek new value
+
+  });
+});
 
 describe("stack exercise", () => {
     it('contains a stack exercise title', () => {
