249b7993f82ad1c5e92c7436eb219a24a50953b3
Daniel Russom <daniel.russom00@gmail.com>
2023-03-28 08:43:12 +0100

    Add clickButton method to test extensions + remove redundant TreeGenerator test

--- src/tree-generator/TreeGenerator.test.tsx
+++ src/tree-generator/TreeGenerator.test.tsx
@@ -1,66 +1,56 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import * as React from "react";
-import { getButton } from '../../testExtensions/screenTestExtensions';
+import { clickButton } from '../../testExtensions/screenTestExtensions';
 import TreeGenerator from "./TreeGenerator";
 
 describe("christmas tree", () => {
-    it('contains a tree size input box', () => {
-      render(<TreeGenerator />);
-    
-      const inputBox = screen.getByLabelText("Tree Size:");
-    
-      expect(inputBox).toBeInTheDocument();
-    });
-    
-    it('contains a generate tree button', () => {
-      render(<TreeGenerator />);
-    
-      const button = getGenerateTreeButton();
-    
-      expect(button).toBeInTheDocument();
-    });
-    
-    it('does not display tree stump before clicking button', () => {
-      render(<TreeGenerator />);
-    
-      const treeDisplay = getTreeDisplay();
-    
-      expect(treeDisplay.textContent).toEqual("")
-    });
-    
-    it.each([
-      ["0", "|"],
-      ["1", "X\n|"],
-      ["2", "X\nXXX\n|"],
-      ["3", "X\nXXX\nXXXXX\n|"],
-    ])
+  it('contains a tree size input box', () => {
+    render(<TreeGenerator />);
+
+    const inputBox = screen.getByLabelText("Tree Size:");
+
+    expect(inputBox).toBeInTheDocument();
+  });
+
+  it('does not display tree stump before clicking button', () => {
+    render(<TreeGenerator />);
+
+    const treeDisplay = getTreeDisplay();
+    expect(treeDisplay.textContent).toEqual("")
+  });
+
+  it.each([
+    ["0", "|"],
+    ["1", "X\n|"],
+    ["2", "X\nXXX\n|"],
+    ["3", "X\nXXX\nXXXXX\n|"],
+  ])
     ('displays expected tree based on tree size', (input, expected) => {
       render(<TreeGenerator />);
-    
+
       const inputBox = screen.getByLabelText("Tree Size:");
       userEvent.type(inputBox, input);
-      fireEvent.click(getGenerateTreeButton());
-      
+      clickGenerateTreeButton();
+
       const treeDisplay = getTreeDisplay();
       expect(treeDisplay.textContent).toEqual(expected)
     });
-    
-    it('splits tree display into multiple lines', () => {
-      render(<TreeGenerator />);
-    
-      fireEvent.click(getGenerateTreeButton());
-      
-      const treeDisplay = getTreeDisplay();
-      expect(treeDisplay).toHaveStyle("white-space: pre-line");
-    });
-    
-    function getGenerateTreeButton(): Element | Node | Document | Window {
-      return getButton("Generate Tree");
-    }
-    
-    function getTreeDisplay() {
-      return screen.getByRole("paragraph");
-    }
-    })
-    
+
+  it('splits tree display into multiple lines', () => {
+    render(<TreeGenerator />);
+
+    clickGenerateTreeButton();
+
+    const treeDisplay = getTreeDisplay();
+    expect(treeDisplay).toHaveStyle("white-space: pre-line");
+  });
+
+  function clickGenerateTreeButton() {
+    clickButton("Generate Tree");
+  }
+
+  function getTreeDisplay() {
+    return screen.getByRole("paragraph");
+  }
+})
--- testExtensions/screenTestExtensions.tsx
+++ testExtensions/screenTestExtensions.tsx
@@ -1,5 +1,9 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 export const getButton = (buttonName : string) => {
     return screen.getByRole('button', { name: buttonName });
+}
+export const clickButton = (buttonName : string) => {
+    userEvent.click(screen.getByRole('button', { name: buttonName }));
 }
