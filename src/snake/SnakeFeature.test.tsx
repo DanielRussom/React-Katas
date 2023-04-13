d75024bf148a90218be2be391eaa9732bf2c24b1
Daniel Russom <daniel.russom00@gmail.com>
2023-04-13 20:00:37 +0100

    Flesh out movement in acceptance test

--- src/snake/SnakeFeature.test.tsx
+++ src/snake/SnakeFeature.test.tsx
@@ -1,18 +1,28 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import SnakeGame from "./SnakeGame";
 import * as React from "react";
+import { clickButton } from "../../testExtensions/screenTestExtensions";
 
 describe("snake eating food feature", () => {
     it("should increase the snake's length and have the snake in the expected position", () => {
         render(<SnakeGame height={7} width={7} />);
 
-        // Set food location
+        // Set food location to 1,1
+       
+        clickButton('Move');
+        clickButton('Move');
+        clickButton('<');
+        clickButton('Move');
+        
+        const gameBoard = screen.getByTitle("GameBoard");
+        const secondRow = gameBoard.childNodes[1];
+        
+        const firstSnakeCell = secondRow.childNodes[1]
+        const secondSnakeCell = secondRow.childNodes[2]
 
-        // Move snake to food
-
-
-
-        // Check snake is longer
-        // Check snake is in expected positions
+        expect(firstSnakeCell).toHaveTextContent("Snake");
+        expect(secondSnakeCell).toHaveTextContent("Snake");
+        
+        expect(screen.getAllByText("Snake").length).toEqual(2);
     });
 });
