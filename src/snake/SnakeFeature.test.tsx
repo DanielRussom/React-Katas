90cb15c00fbd9a50c01e900abef1983c93ee633c
Daniel Russom <daniel.russom00@gmail.com>
2023-04-14 19:48:20 +0100

    Add dummy mock for spawning food

--- src/snake/SnakeFeature.test.tsx
+++ src/snake/SnakeFeature.test.tsx
@@ -2,12 +2,17 @@ import { render, screen } from "@testing-library/react";
 import SnakeGame from "./SnakeGame";
 import * as React from "react";
 import { clickButton } from "../../testExtensions/screenTestExtensions";
+import Position from "./Position";
+
+jest.mock("./FoodSpawner", () => ({
+    getFoodLocation: () => ({
+        position: new Position(1, 1)
+    }),
+}));
 
 describe("snake eating food feature", () => {
     it("should increase the snake's length and have the snake in the expected position", () => {
         render(<SnakeGame height={7} width={7} />);
-
-        // Set food location to 1,1
        
         clickButton('Move');
         clickButton('Move');
