5832b3287e9f4df685a9bd3110974baab3403057
Daniel Russom <daniel.russom00@gmail.com>
2023-04-17 23:05:55 +0100

    Parameterize randomNumber called with tests

--- src/snake/FoodSpawner.test.ts
+++ src/snake/FoodSpawner.test.ts
@@ -18,11 +18,15 @@ describe("Food spawner", () => {
 
         const input = [[""]];
         const result = spawner.pickFoodPosition(input)
-        
-        expect(result).toEqual(new Position(0,0));
+
+        expect(result).toEqual(new Position(0, 0));
     });
 
-    it("picks 0,1", () => {
+    it.each([
+        [[["", ""]], 2],
+        [[["", "", ""]], 3]
+    ])
+    ("picks a position from the expected range", (input, expectedRange) => {
 
         const randomFunction = jest.fn().mockImplementationOnce(() => {
             return 1;
@@ -32,30 +36,11 @@ describe("Food spawner", () => {
 
         const spawner = new FoodSpawner();
 
-        const input = [["", ""]];
         const result = spawner.pickFoodPosition(input)
-        
-        expect(result).toEqual(new Position(0,1));
+
+        expect(result).toEqual(new Position(0, 1));
         expect(randomFunction).toHaveBeenCalledTimes(1);
-        
-        expect(randomFunction).toHaveBeenCalledWith(2);
+
+        expect(randomFunction).toHaveBeenCalledWith(expectedRange);
     });
-
-
-    it("picks a position from 0-2", () => {
-        const randomFunction = jest.fn().mockImplementationOnce(() => {
-            return 0;
-        });
-
-        Random.prototype.getNumber = randomFunction;
-
-        const spawner = new FoodSpawner();
-
-        const input = [["", "", ""]];
-        spawner.pickFoodPosition(input)
-        
-        expect(randomFunction).toHaveBeenCalledTimes(1);
-        
-        expect(randomFunction).toHaveBeenCalledWith(3);
-    })
 });
