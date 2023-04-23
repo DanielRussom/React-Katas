81df02596cfcd3fedc269a473e393ca7f4198c4c
Daniel Russom <daniel.russom00@gmail.com>
2023-04-23 12:58:25 +0100

    Move FoodSpawning tests from SnakeGame.test to Grid.test

--- src/snake/Grid.test.tsx
+++ src/snake/Grid.test.tsx
@@ -1,9 +1,19 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import Grid from "./Grid";
 import * as React from "react";
 import Position from "./Position";
+import { FoodSpawner } from "./FoodSpawner";
+import "../../testExtensions/screenTestExtensions";
+
+jest.mock("./FoodSpawner");
 
 describe("game board", () => {
+    beforeEach(() => {
+        FoodSpawner.prototype.pickFoodPosition = jest.fn().mockImplementation(() => {
+            return new Position(0, 0);
+        });
+    });
+
     it("is a grid", () => {
         render(<Grid height={5} width={5} snakeLocation={new Position(2, 2)} />);
 
@@ -37,5 +47,40 @@ describe("game board", () => {
         const columns = firstRow.childNodes;
 
         expect(columns.length).toEqual(expectedColumns);
+    });
+    
+    describe("Food", () => {
+        it("exists in the grid", () => {
+            render(<Grid height={7} width={7} snakeLocation={new Position(3, 3)}/>);
+
+            const gameBoard = screen.getByTitle("GameBoard");
+            const food = within(gameBoard).getByText("Food");
+
+            expect(food).toBeInTheDocument();
+        });
+
+        it.each([
+            [new Position(1, 1)],
+            [new Position(3, 4)],
+            [new Position(6, 6)]
+        ])
+            ("exists in the expected location", (expectedPosition) => {
+                const pickedFoodFunction = jest.fn().mockImplementationOnce(() => {
+                    return expectedPosition;
+                });
+
+                FoodSpawner.prototype.pickFoodPosition = pickedFoodFunction;
+
+                render(<Grid height={7} width={7} snakeLocation={new Position(3, 3)}/>);
+
+                const expectedFoodLocation = screen.getByTitle("GameBoard").getChildAt(expectedPosition);
+
+                expect(expectedFoodLocation).toHaveTextContent("Food");
+                expect(screen.getAllByText("Food").length).toEqual(1);
+
+                expect(pickedFoodFunction).toHaveBeenCalledTimes(1);
+            });
+
+        // Food respawns after snake collides with it
     });
 });
--- src/snake/SnakeGame.test.tsx
+++ src/snake/SnakeGame.test.tsx
@@ -1,58 +1,19 @@
-import { render, screen, within } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import * as React from "react";
 import { clickButton } from "../../testExtensions/screenTestExtensions";
 import SnakeGame from "./SnakeGame";
 import Position from "./Position";
-import { FoodSpawner } from "./FoodSpawner";
 
-jest.mock("./FoodSpawner");
 
 describe("snake game", () => {
-    beforeEach(() => {
 
-        FoodSpawner.prototype.pickFoodPosition = jest.fn().mockImplementation(() => {
-            return new Position(0, 0);
-        });
+    // Mock out Grid
+    it("game board is a grid", () => {
+        render(<SnakeGame />);
 
-    });
+        const board = screen.getByTitle("GameBoard");
 
-    describe("game board", () => {
-        // Mock out Grid
-        it("is a grid", () => {
-            render(<SnakeGame />);
-
-            const board = screen.getByTitle("GameBoard");
-
-            expect(board).toHaveStyle("display: grid");
-        });
-
-        it.each([[
-            1,
-            3,
-            5
-        ]])
-            ("has the expected row count", (expectedRows) => {
-                render(<SnakeGame height={expectedRows} />);
-
-                const rows = screen.getByTitle("GameBoard").childNodes;
-
-                expect(rows.length).toEqual(expectedRows);
-            });
-
-
-        it.each([[
-            1,
-            2,
-            3
-        ]])("has the expected column count", (expectedColumns) => {
-            render(<SnakeGame width={expectedColumns} />);
-
-            const firstRow = screen.getByTitle("GameBoard").childNodes[0];
-
-            const columns = firstRow.childNodes;
-
-            expect(columns.length).toEqual(expectedColumns);
-        });
+        expect(board).toHaveStyle("display: grid");
     });
 
     describe("snake", () => {
@@ -164,40 +125,5 @@ describe("snake game", () => {
                 expect(screen.getAllByText("Snake").length).toEqual(1);
             });
 
-    });
-
-    describe("Food", () => {
-        it("exists in the grid", () => {
-            render(<SnakeGame height={7} width={7} />);
-
-            const gameBoard = screen.getByTitle("GameBoard");
-            const food = within(gameBoard).getByText("Food");
-
-            expect(food).toBeInTheDocument();
-        });
-
-        it.each([
-            [new Position(1, 1)],
-            [new Position(3, 4)],
-            [new Position(6, 6)]
-        ])
-            ("exists in the expected location", (expectedPosition) => {
-                const pickedFoodFunction = jest.fn().mockImplementationOnce(() => {
-                    return expectedPosition;
-                });
-
-                FoodSpawner.prototype.pickFoodPosition = pickedFoodFunction;
-
-                render(<SnakeGame height={7} width={7} />);
-
-                const expectedFoodLocation = screen.getByTitle("GameBoard").getChildAt(expectedPosition);
-
-                expect(expectedFoodLocation).toHaveTextContent("Food");
-                expect(screen.getAllByText("Food").length).toEqual(1);
-
-                expect(pickedFoodFunction).toHaveBeenCalledTimes(1);
-            });
-
-        // Food respawns after snake collides with it
     });
 });
