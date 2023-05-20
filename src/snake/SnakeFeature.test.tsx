411535afd33ca53539644b992de70d30eeb67972
Daniel Russom <daniel.russom00@gmail.com>
2023-05-20 16:55:55 +0100

    Tidy SnakeFeature file

--- src/snake/SnakeFeature.test.tsx
+++ src/snake/SnakeFeature.test.tsx
@@ -6,26 +6,30 @@ import { Random } from "./Random";
 import Position from "./Position";
 import { FoodToken, SnakeToken } from "./Constants";
 
-
 jest.mock("./Random");
 
 describe("snake eating food feature", () => {
     it("should increase the snake's length and have the snake in the expected position", () => {
-        const randomFunction = 
-        jest.fn()
-        .mockImplementationOnce(() => {
-            return 8; // (1,1)
-        }).mockImplementationOnce(() => {
-            return 22; // (3,3)
-        })
-        //return undefined 
+        const gridHeight = 7;
+        const gridWidth = 7;
+        const firstFoodLocation = new Position(1, 1);
+        const secondFoodLocation = new Position(3, 3);
+
+        const randomFunction =
+            jest.fn()
+                .mockImplementationOnce(() => {
+                    return getGridIndexForFoodSpawn(firstFoodLocation, gridHeight);
+                }).mockImplementationOnce(() => {
+                    return getGridIndexForFoodSpawn(secondFoodLocation, gridHeight, 2);
+                })
+
         Random.prototype.getNumberBelowLimit = randomFunction;
 
-        render(<SnakeGame height={7} width={7} />);
+        render(<SnakeGame height={gridHeight} width={gridWidth} />);
 
         let gameBoard = screen.getByTitle("GameBoard");
-        
-        let expectedFoodLocation = gameBoard.getChildAt(new Position(1,1));
+
+        let expectedFoodLocation = gameBoard.getChildAt(firstFoodLocation);
         expect(expectedFoodLocation).toHaveTextContent(FoodToken);
 
         clickButton('Move');
@@ -34,19 +38,20 @@ describe("snake eating food feature", () => {
         clickButton('Move');
         clickButton('Move');
 
-        gameBoard = screen.getByTitle("GameBoard");
-        
-        const firstExpectedSnakeCell = gameBoard.getChildAt(new Position(0,1));
-        const secondExpectedSnakeCell = gameBoard.getChildAt(new Position(1,1))
+        const firstExpectedSnakeCell = gameBoard.getChildAt(new Position(0, 1));
+        const secondExpectedSnakeCell = gameBoard.getChildAt(new Position(1, 1))
 
         expect(firstExpectedSnakeCell).toHaveTextContent(SnakeToken);
         expect(secondExpectedSnakeCell).toHaveTextContent(SnakeToken);
 
         expect(screen.getAllByText(SnakeToken).length).toEqual(2);
 
-        
-        expectedFoodLocation = gameBoard.getChildAt(new Position(3,3));
-        expect(expectedFoodLocation).toHaveTextContent(FoodToken);
 
+        expectedFoodLocation = gameBoard.getChildAt(secondFoodLocation);
+        expect(expectedFoodLocation).toHaveTextContent(FoodToken);
     });
+
+    function getGridIndexForFoodSpawn(foodPosition, gridHeight, invalidPositionsOffset = 0) {
+        return (foodPosition.y * gridHeight) + foodPosition.x - invalidPositionsOffset
+    }
 });
