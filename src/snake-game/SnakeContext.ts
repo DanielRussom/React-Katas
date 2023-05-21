7fe37468b815db2194b3f8b7ac2710dffe908b69
Daniel Russom <daniel.russom00@gmail.com>
2023-05-21 11:45:01 +0100

    Update reference in SnakeContext

--- src/snake-game/SnakeContext.ts
+++ src/snake-game/SnakeContext.ts
@@ -1,4 +1,4 @@
 import { createContext } from "react";
-import { Snake } from "./Snake";
+import { Snake } from "./snake/Snake";
 
 export const SnakeContext = createContext<{snake: Snake, setSnake: Function}>(undefined!);
